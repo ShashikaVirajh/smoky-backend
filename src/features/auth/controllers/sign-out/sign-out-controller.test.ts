34bed716525009bdd07210fdbc23863133dbeb07
Shashika Virajh <shashikasvka@gmail.com>
2023-02-20 23:32:45 +0530

    Prettier and eslint formattings

--- src/features/auth/controllers/sign-out/sign-out-controller.test.ts
+++ src/features/auth/controllers/sign-out/sign-out-controller.test.ts
@@ -1,31 +1,31 @@
-// import { SignOutController } from '@features/auth/controllers/sign-out/sign-out.controller';
-// import { authMockRequest, authMockResponse } from '@root/mocks/auth.mock';
-// import { Request, Response } from 'express';
+import { SignOutController } from '@features/auth/controllers/sign-out/sign-out.controller';
+import { authMockRequest, authMockResponse } from '@root/mocks/auth.mock';
+import { Request, Response } from 'express';
 
-// const USERNAME = 'Smoky123';
-// const PASSWORD = 'pass1234';
+const USERNAME = 'Smoky123';
+const PASSWORD = 'pass1234';
 
-// describe('sign-out-controller', () => {
-//   it('should set session to null', async () => {
-//     const req: Request = authMockRequest({}, { username: USERNAME, password: PASSWORD }) as Request;
-//     const res: Response = authMockResponse();
+describe('sign-out-controller', () => {
+  it('should set session to null', async () => {
+    const req: Request = authMockRequest({}, { username: USERNAME, password: PASSWORD }) as Request;
+    const res: Response = authMockResponse();
 
-//     await SignOutController.prototype.signOut(req, res);
+    await SignOutController.prototype.signOut(req, res);
 
-//     expect(req.session).toBeNull();
-//   });
+    expect(req.session).toBeNull();
+  });
 
-//   it('should send correct json response', async () => {
-//     const req: Request = authMockRequest({}, { username: USERNAME, password: PASSWORD }) as Request;
-//     const res: Response = authMockResponse();
+  it('should send correct json response', async () => {
+    const req: Request = authMockRequest({}, { username: USERNAME, password: PASSWORD }) as Request;
+    const res: Response = authMockResponse();
 
-//     await SignOutController.prototype.signOut(req, res);
+    await SignOutController.prototype.signOut(req, res);
 
-//     expect(res.status).toHaveBeenCalledWith(200);
-//     expect(res.json).toHaveBeenCalledWith({
-//       message: 'Logout successful',
-//       user: {},
-//       accessToken: ''
-//     });
-//   });
-// });
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({
+      message: 'Logout successful',
+      user: {},
+      accessToken: ''
+    });
+  });
+});
